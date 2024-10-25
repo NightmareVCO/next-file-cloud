834b87ea2adf7d4cdbb8ddb88ed2db7d328a57e1
NightmareVCO <vladimircuriel@outlook.com>
2024-10-25 17:00:05 -0400

    fix(typo): 'org: admin' to 'org:admin'

--- convex/http.ts
+++ convex/http.ts
@@ -47,7 +47,7 @@ http.route({
           await context.runMutation(internal.users.addOrgIdToUser, {
             tokenIdentifier: `https://awake-moose-67.clerk.accounts.dev|${result.data.public_user_data.user_id}`,
             orgId: result.data.organization.id,
-            role: result.data.role === "org: admin" ? "admin" : "member",
+            role: result.data.role === "org:admin" ? "admin" : "member",
           });
           break;
         }
