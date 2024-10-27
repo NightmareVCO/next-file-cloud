0610efb6d51572964bec3a679eac91b8b3b25982
NightmareVCO <vladimircuriel@outlook.com>
2024-10-27 01:48:01 -0400

    refactor(auth): update auth configuration to use environment variable for domain

--- src/ui/components/filesBrowser/FilesBrowser.tsx
+++ src/ui/components/filesBrowser/FilesBrowser.tsx
@@ -1,3 +1,5 @@
+import { Authenticated } from "convex/react";
+
 import { SearchBar } from "@/ui/search-bar";
 
 import FileUploadModal from "../fileUploadModal/FileUploadModal";
@@ -14,7 +16,9 @@ export default function FilesBrowser({
     <div className="flex flex-col items-center justify-between gap-y-4 lg:flex-row">
       <h1 className="text-4xl font-bold">{title}</h1>
       <SearchBar query={query} setQuery={setQuery} />
-      <FileUploadModal onSubmitFunction={createFile} orgId={orgId!} />
+      <Authenticated>
+        <FileUploadModal onSubmitFunction={createFile} orgId={orgId!} />
+      </Authenticated>
     </div>
   );
 }
